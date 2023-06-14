565896078e23e96160dd9b0414d0662dbed4cd15
Tim DeGraffenreid <90429693+timdegraffenreid@users.noreply.github.com>
2023-06-14 13:53:19 -0500

    add nextauth again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
-import { SessionProvider } from 'next-auth/react';
-import { AppProps } from "next/app";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 
-const App = ({ Component, pageProps }: AppProps) => {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
+  
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
-};
+}
 
-export default App;
+export default MyApp;
